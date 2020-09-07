0dfc57e6dde13f679c04c62e4bd045669ebfc6c7
evandasheep <evanb96@gmail.com>
2020-09-07 18:26:45 -0500

    Pattern Headers and Footers
    Not implemented in GUI, planning on levels to more finely control in batch Fills

--- TERGEngine/Pattern.cs
+++ TERGEngine/Pattern.cs
@@ -11,7 +11,9 @@ namespace TERGEngine
         public string Name;
         public string Desc;
         public List<IReference> References;
+        public string[] Header;
         public string[] Body;
+        public string[] Footer;
 
         public Pattern(int i, string name)
         {
@@ -19,10 +21,12 @@ namespace TERGEngine
             Name = name;
             Desc = String.Empty;
             References = new List<IReference>();
+            Header = new string[0];
             Body = new string[] { "[@0]" };
+            Footer = new string[0];
         }
 
-        public string Fill(Engine e)
+        public string Fill(Engine e, bool HeadAndFoot = false)
         {
             //Time to draw straws
             string[] data = new string[References.Count];
@@ -38,6 +42,12 @@ namespace TERGEngine
             for (int i = 0; i < References.Count; i++)
             {
                 output.Replace("[@" + i + "]", data[i]);
+            }
+
+            if(HeadAndFoot)
+            {
+                output.Insert(0, string.Join(Environment.NewLine, Header));
+                output.Append(string.Join(Environment.NewLine, Footer));
             }
 
             return output.ToString();
