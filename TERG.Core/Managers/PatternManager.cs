0579c23c0e96d9a6f367c618cb0737faee9a576e
Evan B <evanb96@gmail.com>
2020-10-03 21:47:54 -0500

    Create PatternManager.cs

---
+++ TERG.Core/Managers/PatternManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TERG.Core.Models;
+
+namespace TERG.Core.Managers
+{
+    internal class PatternManager
+    {
+        private List<Pattern> _patterns;
+
+        internal PatternManager()
+        {
+            _patterns = new List<Pattern>();
+        }
+
+        internal IEnumerable<Pattern> GetAll()
+        {
+            return _patterns;
+        }
+    }
+}
